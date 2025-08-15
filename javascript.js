c1342f637a071fa1b068b3d02a4e0704902ed411
AeroDyn4m1c <148336152+AeroDyn4m1c@users.noreply.github.com>
2025-08-15 22:30:34 +0200

    made all the round logic

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
-const humanScore = 0;
-const computerScore = 0;
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice () {
   let cpuChoice = Math.floor(Math.random() * 3);
@@ -20,5 +20,53 @@ function getHumanChoice () {
 }
 
 function playRound (humanChoice, computerChoice) {
-  
+  if (humanChoice == computerChoice) {
+    return console.log("There is no winner! It's a draw!")
+  }
+
+  switch (humanChoice) {
+    case "rock":
+      switch (computerChoice) {
+        case "paper":
+          computerScore++;
+          return console.log("You lose! Paper beats Rock. The computer score is now " + computerScore);
+
+        case "scissors":
+          humanScore++;
+          return console.log("You win! Rock beats Scissors. The human score is now " + humanScore);
+      }
+
+    case "paper":
+      switch (computerChoice) {
+        case "rock":
+          humanScore++;
+          return console.log("You win! Paper beats Rock. The human score is now " + humanScore);
+
+        case "scissors":
+          computerScore++;
+          return console.log("You lose! Scissors beats Paper. The computer score is now " + computerScore);
+      }
+
+    case "scissors":
+      switch (computerChoice) {
+        case "paper":
+          humanScore++;
+          return console.log("You win! Scissors beats Paper. The human score is now " + humanScore);
+
+        case "rock":
+          computerScore++;
+          return console.log("You lose! Rock beats Scissors. The computer score is now " + computerScore);
+      }
+  }
 }
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+function playGame () {
+  for (let i = 0; i < TimeRanges; i++) {
+    playRound();
+  }
+}
+
+playGame(5);
